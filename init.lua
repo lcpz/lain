bc574619fe7a9dd077d8fe6f972f1763b6936df0
luke bonham <dadasignificanulla@gmail.com>
2013-09-12 17:18:08 +0200

    small fixes

--- init.lua
+++ init.lua
@@ -5,8 +5,7 @@
      Layouts, widgets and utilities for Awesome WM 
                                                    
      Licensed under GNU General Public License v2  
-      * (c) 2013,      Luke Bonham                 
-      * (c) 2010-2012, Peter Hofmann               
+      * (c) 2013, Luke Bonham                      
                                                    
 --]]
 
