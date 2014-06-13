ce9c97aa927a448ca4158104538118c350114b22
luke bonham <dada@archlinux.info>
2014-06-13 09:44:36 +0200

    #52 fix + multiple imap widgets feature added

--- widgets/base.lua
+++ widgets/base.lua
@@ -14,9 +14,9 @@ local setmetatable = setmetatable
 
 -- Basic template for custom widgets 
 -- lain.widgets.base
-local base = {}
 
 local function worker(args)
+    local base     = {}
     local args     = args or {}
     local timeout  = args.timeout or 5
     local cmd      = args.cmd or ""
@@ -37,4 +37,4 @@ local function worker(args)
     return setmetatable(base, { __index = base.widget })
 end
 
-return setmetatable(base, { __call = function(_, ...) return worker(...) end })
+return setmetatable({}, { __call = function(_, ...) return worker(...) end })
