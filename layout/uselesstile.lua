19c89b4f00231aa29019708134fce39a40d4b06e
Samir Benmendil <samir.benmendil@gmail.com>
2015-01-07 18:49:42 +0000

    Allow resizing uselesstiles with the mouse. fix #48

--- layout/uselesstile.lua
+++ layout/uselesstile.lua
@@ -10,14 +10,116 @@
 --]]
 
 local tag       = require("awful.tag")
+local client    = require("awful.client")
 local beautiful = require("beautiful")
 local ipairs    = ipairs
 local math      = { floor = math.floor,
                     max   = math.max,
                     min   = math.min }
 local tonumber  = tonumber
+local capi =
+{
+    mouse = mouse,
+    screen = screen,
+    mousegrabber = mousegrabber
+}
 
 local uselesstile = {}
+
+local function mouse_resize_handler(c, corner, x, y, orientation)
+    local orientation = orientation or "tile"
+    local wa = capi.screen[c.screen].workarea
+    local mwfact = tag.getmwfact()
+    local cursor
+    local g = c:geometry()
+    local offset = 0
+    local x,y
+    if orientation == "tile" then
+        cursor = "cross"
+        if g.height+15 > wa.height then
+            offset = g.height * .5
+            cursor = "sb_h_double_arrow"
+        elseif not (g.y+g.height+15 > wa.y+wa.height) then
+            offset = g.height
+        end
+        capi.mouse.coords({ x = wa.x + wa.width * mwfact, y = g.y + offset })
+    elseif orientation == "left" then
+        cursor = "cross"
+        if g.height+15 >= wa.height then
+            offset = g.height * .5
+            cursor = "sb_h_double_arrow"
+        elseif not (g.y+g.height+15 > wa.y+wa.height) then
+            offset = g.height
+        end
+        capi.mouse.coords({ x = wa.x + wa.width * (1 - mwfact), y = g.y + offset })
+    elseif orientation == "bottom" then
+        cursor = "cross"
+        if g.width+15 >= wa.width then
+            offset = g.width * .5
+            cursor = "sb_v_double_arrow"
+        elseif not (g.x+g.width+15 > wa.x+wa.width) then
+            offset = g.width
+        end
+        capi.mouse.coords({ y = wa.y + wa.height * mwfact, x = g.x + offset})
+    else
+        cursor = "cross"
+        if g.width+15 >= wa.width then
+            offset = g.width * .5
+            cursor = "sb_v_double_arrow"
+        elseif not (g.x+g.width+15 > wa.x+wa.width) then
+            offset = g.width
+        end
+        capi.mouse.coords({ y = wa.y + wa.height * (1 - mwfact), x= g.x + offset })
+    end
+
+    capi.mousegrabber.run(function (_mouse)
+                              for k, v in ipairs(_mouse.buttons) do
+                                  if v then
+                                      local fact_x = (_mouse.x - wa.x) / wa.width
+                                      local fact_y = (_mouse.y - wa.y) / wa.height
+                                      local mwfact
+
+                                      local g = c:geometry()
+
+
+                                      -- we have to make sure we're not on the last visible client where we have to use different settings.
+                                      local wfact
+                                      local wfact_x, wfact_y
+                                      if (g.y+g.height+15) > (wa.y+wa.height) then
+                                          wfact_y = (g.y + g.height - _mouse.y) / wa.height
+                                      else
+                                          wfact_y = (_mouse.y - g.y) / wa.height
+                                      end
+
+                                      if (g.x+g.width+15) > (wa.x+wa.width) then
+                                          wfact_x = (g.x + g.width - _mouse.x) / wa.width
+                                      else
+                                          wfact_x = (_mouse.x - g.x) / wa.width
+                                      end
+
+
+                                      if orientation == "tile" then
+                                          mwfact = fact_x
+                                          wfact = wfact_y
+                                      elseif orientation == "left" then
+                                          mwfact = 1 - fact_x
+                                          wfact = wfact_y
+                                      elseif orientation == "bottom" then
+                                          mwfact = fact_y
+                                          wfact = wfact_x
+                                      else
+                                          mwfact = 1 - fact_y
+                                          wfact = wfact_x
+                                      end
+
+                                      tag.setmwfact(math.min(math.max(mwfact, 0.01), 0.99), tag.selected(c.screen))
+                                      client.setwfact(math.min(math.max(wfact,0.01), 0.99), c)
+                                      return true
+                                  end
+                              end
+                              return false
+                          end, cursor)
+end
 
 local function tile_group(cls, wa, orientation, fact, group)
     -- A useless gap (like the dwm patch) can be defined with
@@ -207,6 +309,9 @@ end
 uselesstile.right = {}
 uselesstile.right.name = "uselesstile"
 uselesstile.right.arrange = tile
+function uselesstile.right.mouse_resize_handler(c, corner, x, y)
+    return mouse_resize_handler(c, corner, x, y)
+end
 
 --- The main tile algo, on left.
 -- @param screen The screen number to tile.
@@ -214,6 +319,9 @@ uselesstile.left = {}
 uselesstile.left.name = "uselesstileleft"
 function uselesstile.left.arrange(p)
     return tile(p, "left")
+end
+function uselesstile.left.mouse_resize_handler(c, corner, x, y)
+    return mouse_resize_handler(c, corner, x, y, "left")
 end
 
 --- The main tile algo, on bottom.
@@ -223,6 +331,9 @@ uselesstile.bottom.name = "uselesstilebottom"
 function uselesstile.bottom.arrange(p)
     return tile(p, "bottom")
 end
+function uselesstile.bottom.mouse_resize_handler(c, corner, x, y)
+    return mouse_resize_handler(c, corner, x, y, "bottom")
+end
 
 --- The main tile algo, on top.
 -- @param screen The screen number to tile.
@@ -231,9 +342,12 @@ uselesstile.top.name = "uselesstiletop"
 function uselesstile.top.arrange(p)
     return tile(p, "top")
 end
+function uselesstile.top.mouse_resize_handler(c, corner, x, y)
+    return mouse_resize_handler(c, corner, x, y, "top")
+end
 
 uselesstile.arrange = uselesstile.right.arrange
+uselesstile.mouse_resize_handler = uselesstile.right.mouse_resize_handler
 uselesstile.name = uselesstile.right.name
 
 return uselesstile
-
