e815650ecceedc5e6b68d8e5571ed8839d1c8d3d
Luke Bonham <dada@archlinux.info>
2015-07-12 11:22:10 +0200

    uniform timers for single-instance widgets; #114

--- widgets/mem.lua
+++ widgets/mem.lua
@@ -23,7 +23,7 @@ local mem = {}
 
 local function worker(args)
     local args     = args or {}
-    local timeout  = args.timeout or 3
+    local timeout  = args.timeout or 2
     local settings = args.settings or function() end
 
     mem.widget = wibox.widget.textbox('')
