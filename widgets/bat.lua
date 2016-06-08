effa8f1dc2e428f876b916efc5b2d8efcbf64863
copycat-killer <dada@archlinux.info>
2016-06-08 11:46:02 +0200

    #204: revert to not hidden as default

--- widgets/bat.lua
+++ widgets/bat.lua
@@ -28,7 +28,7 @@ local function worker(args)
     local timeout   = args.timeout or 30
     local batteries = args.batteries or (args.battery and {args.battery}) or {"BAT0"}
     local ac        = args.ac or "AC0"
-    local notify    = args.notify or "on"
+    local notify    = args.notify or true
     local settings  = args.settings or function() end
 
     bat.widget = wibox.widget.textbox('')
@@ -133,7 +133,7 @@ local function worker(args)
         settings()
 
         -- notifications for low and critical states
-        if bat_now.status == "Discharging" and notify == "on" and bat_now.perc then
+        if notify and bat_now.perc and bat_now.status == "Discharging" then
             local nperc = tonumber(bat_now.perc) or 100
             if nperc <= 5 then
                 bat.id = naughty.notify({
--- widgets/mpd.lua
+++ widgets/mpd.lua
@@ -37,7 +37,7 @@ local function worker(args)
     local music_dir   = args.music_dir or os.getenv("HOME") .. "/Music"
     local cover_size  = args.cover_size or 100
     local default_art = args.default_art or ""
-    local show_notifications = args.show_notifications == true
+    local notify      = args.notify or true
     local followmouse = args.followmouse or false
     local echo_cmd    = args.echo_cmd or "echo"
     local settings    = args.settings or function() end
@@ -91,7 +91,7 @@ local function worker(args)
 
             if mpd_now.state == "play"
             then
-                if show_notifications and mpd_now.title ~= helpers.get_map("current mpd track")
+                if notify and mpd_now.title ~= helpers.get_map("current mpd track")
                 then
                     helpers.set_map("current mpd track", mpd_now.title)
 
