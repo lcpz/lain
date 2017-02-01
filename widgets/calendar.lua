a1ba90261b31ccce1ac2ecb7ca7aedbc20eca441
copycat-killer <dada@archlinux.info>
2017-02-01 21:58:40 +0100

    wiki updated

--- widgets/calendar.lua
+++ widgets/calendar.lua
@@ -68,7 +68,7 @@ function calendar.show(t_out, inc_offset, scr)
 
     helpers.async(f, function(ws)
         fg, bg = calendar.notification_preset.fg, calendar.notification_preset.bg
-        ws = ws:gsub("%c%[+%d+[m]?%s?%d+%c%[+%d+[m]?", markup.bold(markup.color(bg, fg, today)))
+        ws = ws:gsub("%c%[%d+[m]?" .. today .. "%c%[%d+[m]?", markup.bold(markup.color(bg, fg, today)))
         calendar.hide()
         calendar.notification = naughty.notify({
             preset      = calendar.notification_preset,
