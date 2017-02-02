69baf55e0ee911419086829b91211251c1084d77
Calvin Timmer <calvin.timmer@mailbox.org>
2017-02-02 17:56:01 +0100

    Added tooltip to pulseaudio widget

--- widgets/pulseaudio.lua
+++ widgets/pulseaudio.lua
@@ -7,7 +7,7 @@
 --]]
 
 local helpers      = require("lain.helpers")
-local shell        = require("awful.util").shell
+local awful        = require("awful")
 local wibox        = require("wibox")
 local string       = { gmatch = string.gmatch,
                        match  = string.match }
@@ -33,44 +33,49 @@ local function factory(args)
     --  '/device\\.string/ p' prints the line containing 'device.string:'.
     
     local settings    = args.settings or function()
+            widgettext  = volume_now.left .. "%"
+            tooltiptext = volume_now.left .. "% (" .. devicetype .. " " .. volume_now.index .. " \"" .. volume_now.device .. "\")"
             if volume_now.muted == "yes" then
-                widget:set_text("Mute")
-            else
-                widget:set_text(volume_now.left .. "%")
+                widgettext  = "Mute"
+                tooltiptext = "Mute/" .. tooltiptext
             end
+            widget:set_text(widgettext)
+            tooltip:set_text(tooltiptext)
         end
-        
+    
     pulseaudio.widget = wibox.widget.textbox()
-
+    pulseaudio.tooltip = awful.tooltip({ objects = { pulseaudio.widget } })
+    
     function pulseaudio.update()
         if scallback then cmd = scallback() end
-
-        helpers.async({ shell, "-c", cmd }, function(s)
+        
+        helpers.async({ awful.util.shell, "-c", cmd }, function(s)
             volume_now = {
                 index = string.match(s, "index: (%S+)") or "N/A",
                 device = string.match(s, "device.string = \"(%S+)\"") or "N/A",
                 sink   = device,  -- legacy API
                 muted  = string.match(s, "muted: (%S+)") or "N/A"
             }
-
+            
             local ch = 1
             volume_now.channel = {}
             for v in string.gmatch(s, ":.-(%d+)%%") do
                 volume_now.channel[ch] = v
                 ch = ch + 1
             end
-
+            
             volume_now.left  = volume_now.channel[1] or "N/A"
             volume_now.right = volume_now.channel[2] or "N/A"
 
             widget = pulseaudio.widget
+            tooltip = pulseaudio.tooltip
             
             settings()
         end)
     end
-
+    
     helpers.newtimer("pulseaudio", timeout, pulseaudio.update)
-
+    
     return pulseaudio
 end
 
