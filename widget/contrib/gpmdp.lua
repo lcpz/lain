35d0e41d91ebc5e3f7e66e5059f6a3262fb6e047
Greg Flynn <gregflynn42@gmail.com>
2017-07-30 17:05:05 -0400

    refactor gpmdp to table from factory

--- widget/contrib/gpmdp.lua
+++ widget/contrib/gpmdp.lua
@@ -6,100 +6,97 @@
                                                      
 --]]
 
-local helpers             = require("lain.helpers")
-local json                = require("lain.util.dkjson")
-local watch               = require("awful.widget").watch
-local focused             = require("awful.screen").focused
-local naughty             = require("naughty")
-local next, getenv, table = next, os.getenv, table
+local naughty = require("naughty")
 
 -- Google Play Music Desktop infos
 -- lain.widget.contrib.gpmdp
 -- requires: curl
 
-local function factory(args)
-    local gpmdp         = {}
-    local args          = args or {}
-    local timeout       = args.timeout or 2
-    local notify        = args.notify or "off"
-    local hover         = args.hover or "off"
-    local followtag     = args.followtag or false
-    local file_location = args.file_location or
-                          getenv("HOME") .. "/.config/Google Play Music Desktop Player/json_store/playback.json"
-    local settings      = args.settings or function() end
-
-    gpmdp_notification_preset = {
+local gpmdp = {
+    notify        = "on",
+    followtag     = false,
+    file_location = os.getenv("HOME") .. "/.config/Google Play Music Desktop Player/json_store/playback.json",
+    notification_preset = {
         title     = "Now playing",
-        icon_size = 64,
+        icon_size = dpi(128),
         timeout   = 6
-    }
+    },
+    notification  = nil,
+    current_track = nil,
+}
 
-    function gpmdp.hover_on(hover)
-        local gpm_now = gpmdp.latest
-        helpers.set_map("gpmdp_current", gpm_now.title)
+function gpmdp.notification_on()
+    local gpm_now = gpmdp.latest
+    gpmdp.current_track = gpm_now.title
 
-        if followtag then gpmdp_notification_preset.screen = focused() end
-        helpers.async(
-            string.format("curl %s -o /tmp/gpmcover.png", gpm_now.cover_url),
-            function(f)
-                local old_id = nil
-                if gpmdp.notification then old_id = gpmdp.notification.id end
+    if gpmdp.followtag then gpmdp.notification_preset.screen = awful.screen.focused() end
+    awful.spawn.easy_async(string.format("curl %s -o /tmp/gpmcover.png", gpm_now.cover_url), function(stdout)
+        local old_id = nil
+        if gpmdp.notification then old_id = gpmdp.notification.id end
 
-                gpmdp.notification = naughty.notify({
-                    preset = gpmdp_notification_preset,
-                    icon = "/tmp/gpmcover.png",
-                    replaces_id = old_id
-                })
-            end
-        )
-
-    end
-
-    function gpmdp.hover_off()
-        if not gpmdp.notification then return end
-        naughty.destroy(gpmdp.notification)
-        gpmdp.notification = nil
-    end
-
-    helpers.set_map("gpmdp_current", nil)
-
-    local watcher = watch("pidof 'Google Play Music Desktop Player'", timeout, function(widget, stdout)
-        local filelines = helpers.lines_from(file_location)
-        gpm_now = { running = stdout ~= '' }
-
-        if not next(filelines) then
-            gpm_now.running = false
-            gpm_now.playing = false
-        else
-            dict, pos, err = json.decode(table.concat(filelines), 1, nil)
-            gpm_now.artist    = dict.song.artist
-            gpm_now.album     = dict.song.album
-            gpm_now.title     = dict.song.title
-            gpm_now.cover_url = dict.song.albumArt
-            gpm_now.playing   = dict.playing
-        end
-        gpmdp.latest = gpm_now
-
-        gpmdp_notification_preset.text = string.format("%s (%s) - %s", gpm_now.artist, gpm_now.album, gpm_now.title)
-        settings(widget)
-
-        if gpm_now.playing then
-            if notify == "on" and gpm_now.title ~= helpers.get_map("gpmdp_current") then
-                gpmdp.hover_on()
-            end
-        elseif not gpm_now.running then
-            helpers.set_map("gpmdp_current", nil)
-        end
+        gpmdp.notification = naughty.notify({
+            preset = gpmdp.notification_preset,
+            icon = "/tmp/gpmcover.png",
+            replaces_id = old_id
+        })
     end)
-
-    if hover == "on" then
-        watcher:connect_signal("mouse::enter", gpmdp.hover_on)
-        watcher:connect_signal("mouse::leave", gpmdp.hover_off)
-    end
-
-    gpmdp.widget = watcher
-
-    return gpmdp
 end
 
-return factory
+function gpmdp.notification_off()
+    if not gpmdp.notification then return end
+    naughty.destroy(gpmdp.notification)
+    gpmdp.notification = nil
+end
+
+function gpmdp.get_lines(file)
+    local f = io.open(file)
+    if not f then
+        return
+    else
+        f:close()
+    end
+
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+gpmdp.widget = awful.widget.watch("pidof 'Google Play Music Desktop Player'", 2, function(widget, stdout)
+    local filelines = gpmdp.get_lines(gpmdp.file_location)
+    if not filelines then return end -- GPMDP not running?
+
+    gpm_now = { running = stdout ~= '' }
+
+    if not next(filelines) then
+        gpm_now.running = false
+        gpm_now.playing = false
+    else
+        dict, pos, err    = require('dkjson').decode(table.concat(filelines), 1, nil)
+        gpm_now.artist    = dict.song.artist
+        gpm_now.album     = dict.song.album
+        gpm_now.title     = dict.song.title
+        gpm_now.cover_url = dict.song.albumArt
+        gpm_now.playing   = dict.playing
+    end
+    gpmdp.latest = gpm_now
+
+    -- customize here
+    gpmdp_notification_preset.text = string.format("%s (%s) - %s", gpm_now.artist, gpm_now.album, gpm_now.title)
+    widget:set_text(gpm_now.artist .. " - " .. gpm_now.title)
+
+    if gpm_now.playing then
+        if gpmdp.notify == "on" and gpm_now.title ~= gpmdp.current_track then
+            gpmdp.notification_on()
+        end
+    elseif not gpm_now.running then
+        gpmdp.current_track = nil
+    end
+end)
+
+-- add mouse hover
+gpmdp.widget:connect_signal("mouse::enter", gpmdp.notification_on)
+gpmdp.widget:connect_signal("mouse::leave", gpmdp.notification_off)
+
+return gpmdp
