a5b05fa47994ab1c7b15d59b24fb3f5aa2abd877
Luke Bonham <dada@archlinux.info>
2017-08-24 12:49:17 +0200

    weather: set UTC directly #310

--- widget/weather.lua
+++ widget/weather.lua
@@ -11,12 +11,11 @@ local json     = require("lain.util").dkjson
 local focused  = require("awful.screen").focused
 local naughty  = require("naughty")
 local wibox    = require("wibox")
-local math     = { floor    = math.floor }
-local os       = { time     = os.time,
-                   date     = os.date,
-                   difftime = os.difftime }
-local string   = { format   = string.format,
-                   gsub     = string.gsub }
+local math     = { floor  = math.floor }
+local os       = { time   = os.time,
+                   date   = os.date }
+local string   = { format = string.format,
+                   gsub   = string.gsub }
 local tonumber = tonumber
 
 -- OpenWeatherMap
@@ -27,16 +26,11 @@ local function factory(args)
     local weather               = { widget = wibox.widget.textbox() }
     local args                  = args or {}
     local APPID                 = args.APPID or "3e321f9414eaedbfab34983bda77a66e" -- lain default
-    local timeout               = args.timeout or 900   -- 15 min
-    local timeout_forecast      = args.timeout or 86400 -- 24 hrs
+    local timeout               = args.timeout or 60 * 15 -- 15 min
+    local timeout_forecast      = args.timeout or 60 * 60 * 24 -- 24 hrs
     local current_call          = args.current_call  or "curl -s 'http://api.openweathermap.org/data/2.5/weather?id=%s&units=%s&lang=%s&APPID=%s'"
     local forecast_call         = args.forecast_call or "curl -s 'http://api.openweathermap.org/data/2.5/forecast/daily?id=%s&units=%s&lang=%s&cnt=%s&APPID=%s'"
     local city_id               = args.city_id or 0 -- placeholder
-    local utc_offset            = args.utc_offset or
-                                  function ()
-                                      local now = os.time()
-                                      return os.difftime(now, os.time(os.date("!*t", now))) + ((os.date("*t").isdst and 1 or 0) * 3600)
-                                  end
     local units                 = args.units or "metric"
     local lang                  = args.lang or "en"
     local cnt                   = args.cnt or 5
@@ -103,11 +97,10 @@ local function factory(args)
             weather_now, pos, err = json.decode(f, 1, nil)
 
             if not err and type(weather_now) == "table" and tonumber(weather_now["cod"]) == 200 then
-                weather.notification_text = ''
+                weather.notification_text = ""
                 for i = 1, weather_now["cnt"] do
                     weather.notification_text = weather.notification_text ..
                                                 notification_text_fun(weather_now["list"][i])
-
                     if i < weather_now["cnt"] then
                         weather.notification_text = weather.notification_text .. "\n"
                     end
@@ -123,27 +116,10 @@ local function factory(args)
             weather_now, pos, err = json.decode(f, 1, nil)
 
             if not err and type(weather_now) == "table" and tonumber(weather_now["cod"]) == 200 then
-                -- weather icon based on localtime
-                local now     = os.time()
+                local now     = os.time(os.date("!*t"))
                 local sunrise = tonumber(weather_now["sys"]["sunrise"])
                 local sunset  = tonumber(weather_now["sys"]["sunset"])
                 local icon    = weather_now["weather"][1]["icon"]
-                local loc_m   = os.time { year = os.date("%Y"), month = os.date("%m"), day = os.date("%d"), hour = 0 }
-                local offset  = utc_offset()
-                local utc_m   = loc_m - offset
-
-                if offset > 0 and (now - utc_m)>=86400 then
-                    utc_m = utc_m + 86400
-                elseif offset < 0 and (utc_m - now)>=86400 then
-                    utc_m = utc_m - 86400
-                end
-
-                -- if we are 1 day after the GMT, return 1 day back, and viceversa
-                if offset > 0 and loc_m >= utc_m then
-                    now = now - 86400
-                elseif offset < 0 and loc_m <= utc_m then
-                    now = now + 86400
-                end
 
                 if sunrise <= now and now <= sunset then
                     icon = string.gsub(icon, "n", "d")
