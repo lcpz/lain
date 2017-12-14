5686e5d9d0f7c6db834cc3deb04ba78fe2a932f8
vlad-tdot <31749182+vlad-tdot@users.noreply.github.com>
2017-12-14 09:51:33 -0500

    Added wifi.lua

---
+++ widget/wifi.lua
@@ -0,0 +1,41 @@
+local helpers  = require("lain.helpers")
+local wibox    = require("wibox")
+local open     = io.open
+local tonumber = tonumber
+
+-- wifi link info
+-- lain.widget.wifi
+-- most code copied from awesome-copycats lain
+-- plagiarized by vlad-tdot (kvantum)
+
+local function factory(args)
+    local wifi	   = { widget = wibox.widget.textbox() }
+    local args	   = args or {}
+    local timeout  = args.timeout or 2
+    local wififile = args.wififile or "/proc/net/wireless"
+    local settings = args.settings or function() end
+	
+    function wifi.update()
+	local f = open(wififile)
+	local wcontent
+	if f then
+	    wcontent = f:read("*all")
+	    f:close()
+	end
+
+	local pattern = " (%-%d+)%."
+
+	
+	wifi_link = wcontent:match(pattern) or "--"
+	widget = wifi.widget
+	settings()
+    end
+	
+    helpers.newtimer("wifi", timeout, wifi.update)
+    
+    return wifi
+
+end -- function factory
+
+return factory
+
