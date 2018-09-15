be69daf9644b137784789878f91b2479c09afc69
Luca CPZ <luca.cpz@gmail.com>
2018-09-15 10:21:37 +0200

    util.markup: fixed typos

--- util/markup.lua
+++ util/markup.lua
@@ -25,37 +25,37 @@ function markup.small(text)     return format("<small>%s</small>", text) end
 
 -- Set the font
 function markup.font(font, text)
-  return format("<span font='%s'>%s</span>", font, text)
+    return format("<span font='%s'>%s</span>", font, text)
 end
 
 -- Set the foreground
 function markup.fg.color(color, text)
-  return format("<span foreground='%s'>%s</span>", fg, bg, text)
+    return format("<span foreground='%s'>%s</span>", color, text)
 end
 
 -- Set the background
 function markup.bg.color(color, text)
-  return format("<span background='%s'>%s</span>", fg, bg, text)
+    return format("<span background='%s'>%s</span>", color, text)
 end
 
 -- Set foreground and background
 function markup.color(fg, bg, text)
-  return format("<span foreground='%s' background='%s'>%s</span>", fg, bg, text)
+    return format("<span foreground='%s' background='%s'>%s</span>", fg, bg, text)
 end
 
 -- Set font and foreground
 function markup.fontfg(font, fg, text)
-  return format("<span font='%s' foreground='%s'>%s</span>", font, fg, text)
+    return format("<span font='%s' foreground='%s'>%s</span>", font, fg, text)
 end
 
 -- Set font and background
 function markup.fontbg(font, bg, text)
-  return format("<span font='%s' background='%s'>%s</span>", font, bg, text)
+    return format("<span font='%s' background='%s'>%s</span>", font, bg, text)
 end
 
 -- Set font, foreground and background
 function markup.fontcolor(font, fg, bg, text)
-  return format("<span font='%s' foreground='%s' background='%s'>%s</span>", font, fg, bg, text)
+    return format("<span font='%s' foreground='%s' background='%s'>%s</span>", font, fg, bg, text)
 end
 
 -- link markup.{fg,bg}(...) calls to markup.{fg,bg}.color(...)
