8112df667d6c56dfec90695341f22af8718a6eb6
lcpz <luca.cpz@gmail.com>
2018-11-24 14:08:04 +0000

    lain.imap: various improvements
    1. allow special chars in password
    2. make it gmail/yandex compliant
    3. fetch additional data (MESSAGES and RECENT)
    4. support DBus Secret Service authentication method

--- widget/imap.lua
+++ widget/imap.lua
@@ -17,22 +17,28 @@ local tonumber = tonumber
 -- lain.widget.imap
 
 local function factory(args)
-    local imap      = { widget = wibox.widget.textbox() }
-    local args      = args or {}
-    local server    = args.server
-    local mail      = args.mail
-    local password  = args.password
-    local port      = args.port or 993
-    local timeout   = args.timeout or 60
-    local is_plain  = args.is_plain or false
-    local followtag = args.followtag or false
-    local notify    = args.notify or "on"
-    local settings  = args.settings or function() end
+    local imap       = { widget = wibox.widget.textbox() }
+    local args       = args or {}
+    local server     = args.server
+    local mail       = args.mail
+    local password   = args.password
+    local port       = args.port or 993
+    local timeout    = args.timeout or 60
+    local pwdtimeout = args.pwdtimeout or 10
+    local is_plain   = args.is_plain or false
+    local followtag  = args.followtag or false
+    local notify     = args.notify or "on"
+    local settings   = args.settings or function() end
 
     local head_command = "curl --connect-timeout 3 -fsm 3"
-    local request = "-X 'SEARCH (UNSEEN)'"
+    local request = "-X 'STATUS INBOX (MESSAGES RECENT UNSEEN)'"
 
     if not server or not mail or not password then return end
+
+    mail_notification_preset = {
+        icon     = helpers.icons_dir .. "mail.png",
+        position = "top_left"
+    }
 
     helpers.set_map(mail, 0)
 
@@ -40,40 +46,45 @@ local function factory(args)
         if type(password) == "string" or type(password) == "table" then
             helpers.async(password, function(f) password = f:gsub("\n", "") end)
         elseif type(password) == "function" then
-            local p = password()
+            imap.pwdtimer = helpers.newtimer(mail .. "-password", pwdtimeout, function()
+                local retrieved_password, try_again = password()
+                if not try_again then
+                    imap.pwdtimer:stop() -- stop trying to retrieve
+                    password = retrieved_password or "" -- failsafe
+                end
+            end, true, true)
         end
     end
 
-    function update()
-        mail_notification_preset = {
-            icon     = helpers.icons_dir .. "mail.png",
-            position = "top_left"
-        }
+    function imap.update()
+        -- do not update if the password has not been retrieved yet
+        if type(password) ~= "string" then return end
 
-        if followtag then
-            mail_notification_preset.screen = awful.screen.focused()
-        end
-
-        local curl = string.format("%s --url imaps://%s:%s/INBOX -u %s:%q %s -k",
+        local curl = string.format("%s --url imaps://%s:%s/INBOX -u %s:'%s' %s -k",
                      head_command, server, port, mail, password, request)
 
         helpers.async(curl, function(f)
-            mailcount = tonumber(f:match("UNSEEN (%d+)"))
+            imap_now = { ["MESSAGES"] = 0, ["RECENT"] = 0, ["UNSEEN"] = 0 }
+
+            for s, d in f:gmatch("(%w+)%s+(%d+)") do imap_now[s] = tonumber(d) end
+            mailcount = imap_now["UNSEEN"] -- backwards compatibility
+            helpers.set_map(mail, mailcount)
             widget = imap.widget
+
             settings()
 
             if notify == "on" and mailcount and mailcount >= 1 and mailcount > helpers.get_map(mail) then
-                local nt = mail .. " has <b>" .. mailcount .. "</b> new message"
-                if mailcount >= 1 then nt = nt .. "s" end
-                naughty.notify { preset = mail_notification_preset, text = nt }
+                if followtag then mail_notification_preset.screen = awful.screen.focused() end
+                naughty.notify {
+                    preset = mail_notification_preset,
+                    text   = string.format("%s has <b>%d</b> new message%s", mail, mailcount, mailcount == 1 and "" or "s")
+                }
             end
-
-            helpers.set_map(mail, mailcount)
         end)
 
     end
 
-    imap.timer = helpers.newtimer(mail, timeout, update, true, true)
+    imap.timer = helpers.newtimer(mail, timeout, imap.update, true, true)
 
     return imap
 end
