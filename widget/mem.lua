d132e9e6d99ad9e9ce7821c58de37fed14824bed
staticssleever668 <64694940+staticssleever668@users.noreply.github.com>
2020-12-18 15:49:37 +0300

    widget mem: move things around

--- widget/mem.lua
+++ widget/mem.lua
@@ -5,10 +5,9 @@
       * (c) 2010-2012, Peter Hofmann
 
 --]]
-
-local helpers              = require("lain.helpers")
-local wibox                = require("wibox")
 local gmatch, lines, floor = string.gmatch, io.lines, math.floor
+local wibox                = require("wibox")
+local helpers              = require("lain.helpers")
 
 -- Memory usage (ignoring caches)
 -- lain.widget.mem
@@ -18,25 +17,26 @@ local function factory(args)
 
     local mem      = { widget = args.widget or wibox.widget.textbox() }
 
-    mem.timeout    = args.timeout or 2
     mem.settings   = args.settings or function() end
+    mem.timeout    = args.timeout or 2
 
     function mem.update()
         for line in lines("/proc/meminfo") do
             for k, v in gmatch(line, "([%a]+):[%s]+([%d]+).+") do
-                if     k == "MemTotal"     then mem.total = floor(v / 1024 + 0.5)
-                elseif k == "MemFree"      then mem.free  = floor(v / 1024 + 0.5)
-                elseif k == "Buffers"      then mem.buf   = floor(v / 1024 + 0.5)
+                if k == "Buffers"      then mem.buf   = floor(v / 1024 + 0.5)
                 elseif k == "Cached"       then mem.cache = floor(v / 1024 + 0.5)
-                elseif k == "SwapTotal"    then mem.swap  = floor(v / 1024 + 0.5)
-                elseif k == "SwapFree"     then mem.swapf = floor(v / 1024 + 0.5)
+                elseif k == "MemFree"      then mem.free  = floor(v / 1024 + 0.5)
+                elseif k == "MemTotal"     then mem.total = floor(v / 1024 + 0.5)
                 elseif k == "SReclaimable" then mem.srec  = floor(v / 1024 + 0.5)
+                elseif k == "SwapFree"     then mem.swapf = floor(v / 1024 + 0.5)
+                elseif k == "SwapTotal"    then mem.swap  = floor(v / 1024 + 0.5)
                 end
             end
         end
 
-        mem.used = mem.total - mem.free - mem.buf - mem.cache - mem.srec
         mem.swapused = mem.swap - mem.swapf
+        mem.used = mem.total - mem.free - mem.buf - mem.cache - mem.srec
+
         mem.perc = math.floor(mem.used / mem.total * 100)
 
         mem.settings()
