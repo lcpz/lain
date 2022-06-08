b3ab50d4a8c7c667599ab72e5679647eb56dcecc
thomas <thomas.croft@pm.me>
2022-06-08 13:22:50 -0600

    quit hopefully

--- .tests/main.lua
+++ .tests/main.lua
@@ -562,3 +562,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+--
+--
+--
+awesome.quit()
