f1310d32dc8217256ac2eb3f68be6039af1c3fbd
Janne Veteläinen <janne.vetelainen@elisanet.fi>
2022-09-11 18:17:17 +0300

    Remove confusing comments

--- widget/contrib/nvtemp.lua
+++ widget/contrib/nvtemp.lua
@@ -1,5 +1,4 @@
 local helpers = require("lain.helpers")
---local shell   = require("awful.util").shell
 local wibox   = require("wibox")
 local string  = string
 
@@ -8,44 +7,22 @@ local string  = string
 
 local function factory(args)
     args           = args or {}
-    --local alsa     = { widget = args.widget or wibox.widget.textbox() }
     local nvtemp     = { widget = args.widget or wibox.widget.textbox() }
     local timeout  = args.timeout or 5
     local settings = args.settings or function() end
 
-    --alsa.cmd           = args.cmd or "amixer"
-    nvtemp.cmd           = args.cmd or "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader"
-    --alsa.channel       = args.channel or "Master"
-    --alsa.togglechannel = args.togglechannel
+    nvtemp.cmd           = "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader"
 
-    --local format_cmd = string.format("%s get %s", alsa.cmd, alsa.channel)
-
-    --[[
-    if alsa.togglechannel then
-        format_cmd = { shell, "-c", string.format("%s get %s; %s get %s",
-        alsa.cmd, alsa.channel, alsa.cmd, alsa.togglechannel) }
-    end
-    --]]
-
-    --alsa.last = {}
-
-    --function alsa.update()
     function nvtemp.update()
-        --helpers.async(format_cmd, function(mixer)
         helpers.async(nvtemp.cmd, function(temp)
-            local l = temp
-            l = tonumber(l)
-            --if alsa.last.level ~= l or alsa.last.status ~= s then
-                --volume_now = { level = l, status = s }
-            nvtemp_now = l
+            temp = tonumber(temp)
+            nvtemp_now = temp
             widget = nvtemp.widget
             settings()
-            --alsa.last = volume_now
-            --end
         end)
     end
 
-    helpers.newtimer(string.format("foo"), timeout, nvtemp.update)
+    helpers.newtimer(string.format("nvidia_temp"), timeout, nvtemp.update)
 
     return nvtemp
 end
